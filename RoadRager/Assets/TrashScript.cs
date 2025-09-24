4b724931693a2629de07258e88aa5b96d6831595
AidanKleine <aidankleine@outlook.com>
2025-09-24 16:53:20 -0400

    Adding beginnings of trash objects
    Created test scene, created object and working physics material, starting on code

---
+++ RoadRager/Assets/TrashScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TrashScript : MonoBehaviour
+{
+    Rigidbody rb;
+    float spd 10f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnAwake()
+    {
+        rb.AddForce(transform.forward * spd, ForceMode.Impulse);
+    }
+
+}
