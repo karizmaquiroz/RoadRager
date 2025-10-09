21b93c40744ce7e7cdeeed49e94570f2f11e1dbd
AidanKleine <aidankleine@outlook.com>
2025-10-09 14:12:48 -0400

    made a truck variable public

--- RoadRager/Assets/Truck.cs
+++ RoadRager/Assets/Truck.cs
@@ -8,10 +8,11 @@ public class Truck : MonoBehaviour
     public List<GameObject> spawnPts;
     public GameObject trashPrefab;
     int spawnInt = 0; //temp var, figure out how to use clock later
+    public int spawnIntLim;
 
     void Update()
     {
-        if (spawnInt == 900)
+        if (spawnInt == spawnIntLim)
         {
             Instantiate(trashPrefab, spawnPts[Random.Range(0, spawnPts.Count)].transform.position, Quaternion.identity);
             spawnInt = 0;
