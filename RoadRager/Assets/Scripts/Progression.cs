0113e6bdfa2d3a29e18fbc123be022c89198f96d
LullabySiren <anaiyatucker@gmail.com>
2025-10-09 23:32:33 -0400

    Progression manager
    -added a basic framework for the level progression

---
+++ RoadRager/Assets/Scripts/Progression.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Progression : MonoBehaviour
+{
+    float playerDistance;
+    float distance;
+    int levelCount = 1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerDistance = 0;
+        distance = 500.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckIfDistanceCrossed();
+    }
+
+    void CalculateDistance()
+    {
+        distance *= levelCount;
+    }
+
+    void CheckIfDistanceCrossed()
+    {
+        if(playerDistance == distance)
+        {
+            levelCount++;
+            CalculateDistance();
+            ChangeLevel();
+
+        }
+    }
+
+    void ChangeLevel()
+    {
+        //grab some object unique to the level scene, so that we know what scene we are in.
+        //then, choose a level at random that ISN'T the current one. 
+    }
+}
