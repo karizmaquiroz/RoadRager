b4dd0d9b71ed8fc179621daf25ae3d9f28c03278
Frame-solid <102352741+Frame-solid@users.noreply.github.com>
2025-10-30 11:47:28 -0400

    Bandaid Fixed Inputs

--- RoadRager/Assets/Scripts/TextureRoll.cs
+++ RoadRager/Assets/Scripts/TextureRoll.cs
@@ -5,6 +5,7 @@ public class TextureRoll : MonoBehaviour
     public bool isLeftCity;
     public bool isRightCity;
     float scrollSpeed = 3f;
+    float cityScrollSpeed = .5f;
     float offset;
     float rotate;
     MeshRenderer render;
@@ -19,10 +20,10 @@ public class TextureRoll : MonoBehaviour
 void Update()
     {
         if (isLeftCity) {
-            offset += (Time.deltaTime * scrollSpeed) / 10.0f;
+            offset += (Time.deltaTime * cityScrollSpeed) / 10.0f;
             render.material.SetTextureOffset("_BaseMap", new Vector2(offset, 0));
         } else if (isRightCity) {
-            offset += (Time.deltaTime * scrollSpeed) / 10.0f;
+            offset += (Time.deltaTime * cityScrollSpeed) / 10.0f;
             render.material.SetTextureOffset("_BaseMap", new Vector2(-offset, 0));
         }
         else {
