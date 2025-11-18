76cd6ad86f844d95e70b0e8ea9fabf09630cb3a7
LullabySiren <anaiyatucker@gmail.com>
2025-11-18 16:48:59 -0500

    MoneyMagnetization

---
+++ RoadRager/Assets/Scripts/MoneyMagnetization.cs
@@ -0,0 +1,45 @@
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+
+public class MoneyMagnetization : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    GameObject player;
+  
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float dist = Vector3.Distance(transform.position, player.transform.position);
+
+        if (dist < 5)
+        {
+            Debug.Log("Moving towards the player.");
+            float step = 25.0f * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
