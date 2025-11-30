5fbf4ceb987db7f16fee56b4cf3cb0e7a6aa6454
karizmaquiroz <quiroz.karizma@gmail.com>
2025-11-30 14:10:03 -0500

    Scripts for the shop and accessing items

---
+++ RoadRager/Assets/Scripts/Managers/SaveManager.cs
@@ -0,0 +1,92 @@
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+
+//update script to include the upgrades buy button and price; 
+
+
+
+//attached to the SaveManager gameobject
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager instance {  get; private set; }
+
+    //what we want to save
+
+    public int currentItem; //accessory (non consumable)
+    public int money; //in game currency
+    public bool[] carItemUnlocked = new bool[5] { true, false, false, false, false }; //accessory already purchased (non consumable) item goes from non to diff color dice
+
+
+
+
+    public void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else 
+        { 
+            instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+        Load();
+    }
+
+    public void Load()
+    {
+        if(File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+            PlayerData_Storage data = (PlayerData_Storage)bf.Deserialize(file);
+
+
+            money = data.money;
+            currentItem = data.currentItem;
+            carItemUnlocked = data.carItemUnlocked;
+
+
+            if (data.carItemUnlocked == null)
+                carItemUnlocked = new bool[5] { true, false, false, false, false };
+
+
+
+            file.Close();
+
+
+        }
+       
+    }
+
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+        PlayerData_Storage data = new PlayerData_Storage();
+
+
+        data.money = money;
+        data.currentItem = currentItem;
+        data.carItemUnlocked = carItemUnlocked;
+
+
+        bf.Serialize(file, data);
+        file.Close();
+
+    }
+
+
+}
+
+[Serializable]
+class PlayerData_Storage
+{
+    public int currentItem;
+    public int money;
+    public bool[] carItemUnlocked;
+}
---
+++ RoadRager/Assets/Scripts/ShopScript/MoneyAdd.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+
+
+//attached to the SaveManager gameobject
+
+public class MoneyAdd : MonoBehaviour
+{
+
+    //update method to add money
+
+    //SaveManager.instance.money += 0000;
+    //SaveManager.instance.Save();
+
+
+
+
+}
